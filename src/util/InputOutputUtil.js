f4e2990e08ddc1dedef4bad9d270403cb6dbf1fa
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-08-07 14:17:53 +0200

    feat(project): extract all variables from root element
    Related to bpmn-io/bpmn-js-properties-panel#342

---
+++ src/util/InputOutputUtil.js
@@ -0,0 +1,79 @@
+import { filter } from 'min-dash';
+
+/**
+ * Get a inputOutput from the business object
+ *
+ * @param {ModdleElement} element
+ *
+ * @return {ModdleElement} the inputOutput object
+ */
+export function getInputOutput(element) {
+  return (getElements(element, 'camunda:InputOutput') || [])[0];
+}
+
+/**
+ * Return all input parameters existing in the business object, and
+ * an empty array if none exist.
+ *
+ * @param  {ModdleElement} element
+ *
+ * @return {Array<ModdleElement>} a list of input parameter objects
+ */
+export function getInputParameters(element) {
+  return getParameters(element, 'inputParameters');
+}
+
+/**
+ * Return all output parameters existing in the business object, and
+ * an empty array if none exist.
+ *
+ * @param  {ModdleElement} element
+ * @param  {boolean} insideConnector
+ *
+ * @return {Array<ModdleElement>} a list of output parameter objects
+ */
+export function getOutputParameters(element) {
+  return getParameters(element, 'outputParameters');
+}
+
+// helpers //////////
+
+function getElements(element, type, property) {
+  const elems = getExtensionElements(element, type) || [];
+
+  return !property ? elems : (elems[0] || {})[property] || [];
+}
+
+function getParameters(element, property) {
+  const inputOutput = getInputOutput(element);
+
+  return (inputOutput && inputOutput.get(property)) || [];
+}
+
+function getExtensionElements(element, type) {
+  const extensionElements = element.get('extensionElements');
+
+  if (typeof extensionElements !== 'undefined') {
+    const extensionValues = extensionElements.get('values');
+
+    if (typeof extensionValues !== 'undefined') {
+      const elements = filter(extensionValues, function(value) {
+        return is(value, type);
+      });
+
+      if (elements.length) {
+        return elements;
+      }
+    }
+  }
+}
+
+// helpers //////////
+
+function is(element, type) {
+  return (
+    element &&
+    typeof element.$instanceOf === 'function' &&
+    element.$instanceOf(type)
+  );
+}
---
+++ test/spec/ProcessVariablesHelperSpec.js
@@ -0,0 +1,234 @@
+import chai, { expect } from 'chai';
+
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+import { sortBy } from 'min-dash';
+
+import BpmnModdle from 'bpmn-moddle';
+
+import CamundaBpmnModdle from 'camunda-bpmn-moddle/resources/camunda';
+
+import fs from 'fs';
+
+import {
+  getProcessVariables
+} from '../../src/ProcessVariablesHelper';
+
+
+describe('providers/camunda/util - getProcessVariables', function() {
+
+  describe('#getProcessVariables', function() {
+    it('should extract variables - simple process', async function() {
+
+      // given
+      const xml = read('test/fixtures/simple.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+      ]);
+    });
+
+
+    it('should extract variables - with sub process', async function() {
+
+      // given
+      const xml = read('test/fixtures/sub-process.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable3', origin: ['Task_2'], scope: 'Process_1' },
+      ]);
+    });
+
+
+    it('should extract variables - with sub process (duplicated vars)', async function() {
+
+      // given
+      const xml = read('test/fixtures/sub-process-duplicates.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1', 'Task_2'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable3', origin: ['Task_2'], scope: 'Process_1' },
+      ]);
+    });
+
+
+    it('should extract variables - with sub process (own scope)', async function() {
+
+      // given
+      const xml = read('test/fixtures/sub-process-own-scope.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable3', origin: ['Task_2'], scope: 'SubProcess_1' },
+      ]);
+    });
+
+
+    it('should extract variables - with sub process (own scope, duplicates)', async function() {
+
+      // given
+      const xml = read('test/fixtures/sub-process-own-scope-duplicates.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_2'], scope: 'SubProcess_1' },
+        { name: 'variable3', origin: ['Task_2'], scope: 'SubProcess_1' },
+      ]);
+    });
+
+
+    it('should extract variables - sub process (output mapping)', async function() {
+
+      // given
+      const xml = read('test/fixtures/sub-process-output-mapping.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable3', origin: ['SubProcess_1'], scope: 'Process_1' },
+        { name: 'variable3', origin: ['Task_2'], scope: 'SubProcess_1' },
+      ]);
+    });
+
+
+    it('should extract variables - nested sub processes', async function() {
+
+      // given
+      const xml = read('test/fixtures/nested-sub-process.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      // then
+      expect(variables).to.eql([
+        { name: 'variable1', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable2', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable3', origin: ['Task_2'], scope: 'SubProcess_1' },
+      ]);
+    });
+
+
+    it('should extract variables - complex', async function() {
+
+      // given
+      const xml = read('test/fixtures/complex.bpmn');
+
+      const definitions = await parse(xml);
+
+      const rootElement = getRootElement(definitions);
+
+      // when
+      const variables = getProcessVariables(rootElement);
+
+      const sortedVariables = sortVariablesByName(variables);
+
+      // then
+      expect(sortedVariables).to.eql([
+        { name: 'variable01', origin: ['Task_1'], scope: 'Process_1' },
+        { name: 'variable02', origin: ['Event_2'], scope: 'Process_1' },
+        { name: 'variable03', origin: ['SubProcess_1'], scope: 'Process_1' },
+        { name: 'variable03', origin: ['Task_3'], scope: 'SubProcess_1' },
+        { name: 'variable04', origin: ['Task_4'], scope: 'Process_1' },
+        { name: 'variable05', origin: ['Task_5'], scope: 'Process_1' },
+        { name: 'variable06', origin: ['Event_5'], scope: 'Process_1' },
+        { name: 'variable07', origin: ['Event_5'], scope: 'Process_1' },
+        { name: 'variable08', origin: ['Task_7'], scope: 'Process_1' },
+        { name: 'variable09', origin: ['Event_6'], scope: 'Process_1' },
+        { name: 'variable10', origin: ['Event_8'], scope: 'SubProcess_2' },
+        { name: 'variable11', origin: ['Event_8'], scope: 'Process_1' },
+        { name: 'variable12', origin: ['Task_10'], scope: 'SubProcess_2' },
+        { name: 'variable13', origin: ['Task_10'], scope: 'SubProcess_3' },
+        { name: 'variable14', origin: ['Task_10'], scope: 'Process_1' },
+        { name: 'variable15', origin: ['Task_11'], scope: 'Process_1' },
+        { name: 'variable16', origin: ['Task_12'], scope: 'Process_1' },
+      ]);
+    });
+  });
+
+});
+
+// helpers //////////
+
+function getRootElement(definitions) {
+  return definitions.get('rootElements')[0];
+}
+
+async function parse(xml) {
+  const moddle = new BpmnModdle({
+    camunda: CamundaBpmnModdle,
+  });
+
+  const { rootElement: definitions } = await moddle.fromXML(xml);
+
+  return definitions;
+}
+
+function read(path, encoding = 'utf8') {
+  return fs.readFileSync(path, encoding);
+}
+
+function sortVariablesByName(variables) {
+  return sortBy(variables, function(variable) {
+    return variable.name;
+  });
+}
