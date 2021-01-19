f043950f0d3d8816b0b5f3cd5ec9f28afe9ecefd
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-01-19 13:25:10 +0100

    feat: extract event definition variables
    Closes #9

---
+++ test/spec/extractors/extractEventDefinitionVariablesSpec.js
@@ -0,0 +1,132 @@
+import { expect } from 'chai';
+
+import { map } from 'min-dash';
+
+import BpmnModdle from 'bpmn-moddle';
+
+import CamundaBpmnModdle from 'camunda-bpmn-moddle/resources/camunda';
+
+import fs from 'fs';
+
+import extractVariables from '../../../src/extractors/extractEventDefinitionVariables';
+
+import { selfAndAllFlowElements } from '../../../src/util/ElementsUtil';
+
+
+describe('extractors - event definition variables', function() {
+
+  it('should extract variables from error code variable', async function() {
+
+    // given
+    const xml = read('test/fixtures/error-code-variable.bpmn');
+
+    const definitions = await parse(xml);
+
+    const rootElement = getRootElement(definitions);
+
+    const elements = selfAndAllFlowElements([ rootElement ], false);
+
+    // when
+    const variables = extractVariables({
+      elements,
+      containerElement: rootElement,
+      processVariables: []
+    });
+
+    // then
+    expect(convertToTestable(variables)).to.eql([
+      { name: 'variable1', origin: ['ErrorEvent_1'], scope: 'Process_1' },
+      { name: 'variable2', origin: ['ErrorEvent_2'], scope: 'Process_1' }
+    ]);
+
+  });
+
+
+  it('should extract variables from error message variable', async function() {
+
+    // given
+    const xml = read('test/fixtures/error-message-variable.bpmn');
+
+    const definitions = await parse(xml);
+
+    const rootElement = getRootElement(definitions);
+
+    const elements = selfAndAllFlowElements([rootElement], false);
+
+    // when
+    const variables = extractVariables({
+      elements,
+      containerElement: rootElement,
+      processVariables: []
+    });
+
+    // then
+    expect(convertToTestable(variables)).to.eql([
+      { name: 'variable1', origin: ['ErrorEvent_1'], scope: 'Process_1' },
+      { name: 'variable2', origin: ['ErrorEvent_2'], scope: 'Process_1' }
+    ]);
+
+  });
+
+
+  it('should extract variables from escalation code variable', async function() {
+
+    // given
+    const xml = read('test/fixtures/escalation-code-variable.bpmn');
+
+    const definitions = await parse(xml);
+
+    const rootElement = getRootElement(definitions);
+
+    const elements = selfAndAllFlowElements([rootElement], false);
+
+    // when
+    const variables = extractVariables({
+      elements,
+      containerElement: rootElement,
+      processVariables: []
+    });
+
+    // then
+    expect(convertToTestable(variables)).to.eql([
+      { name: 'variable1', origin: ['EscalationEvent_1'], scope: 'Process_1' },
+      { name: 'variable2', origin: ['EscalationEvent_2'], scope: 'Process_1' }
+    ]);
+
+  });
+
+});
+
+
+// helpers //////////
+
+function getRootElement(definitions) {
+  return definitions.get('rootElements')[0];
+}
+
+async function parse(xml) {
+  const moddle = new BpmnModdle({
+    camunda: CamundaBpmnModdle,
+  });
+
+  const { rootElement: definitions } = await moddle.fromXML(xml);
+
+  return definitions;
+}
+
+function read(path, encoding = 'utf8') {
+  return fs.readFileSync(path, encoding);
+}
+
+// converts the variables list from full moddle elements to only id, for better testability
+function convertToTestable(variables) {
+  return map(variables, function(variable) {
+    return {
+      name: variable.name,
+      origin: map(variable.origin, function(origin) {
+        return origin.id;
+      }),
+      scope: variable.scope.id
+    };
+  });
+}
