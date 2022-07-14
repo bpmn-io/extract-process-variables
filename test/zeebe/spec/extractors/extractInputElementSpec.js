ac7fcaa39cc7599d7e5c81bd2dd41de72ec82f82
Martin Stamm <martin.stamm@camunda.com>
2022-07-14 11:11:59 +0200

    test: add variable tests for zeebe

---
+++ test/zeebe/spec/extractors/extractInputElementSpec.js
@@ -0,0 +1,76 @@
+import { expect } from 'chai';
+
+import { map } from 'min-dash';
+
+import BpmnModdle from 'bpmn-moddle';
+
+import ZeebeModdle from 'zeebe-bpmn-moddle/resources/zeebe';
+
+import fs from 'fs';
+
+import extractVariables from '../../../../src/zeebe/extractors/extractInputElement';
+
+import { selfAndAllFlowElements } from '../../../../src/zeebe/util/ElementsUtil';
+
+
+describe('zeebe/extractors - input element', function() {
+
+  it('should extract variables from input elements', async function() {
+
+    // given
+    const xml = read('test/zeebe/fixtures/mi-subprocess.bpmn');
+
+    const definitions = await parse(xml);
+
+    const rootElement = getRootElement(definitions);
+
+    const elements = selfAndAllFlowElements([rootElement], false);
+
+    // when
+    const variables = extractVariables({
+      elements,
+      containerElement: rootElement,
+      processVariables: []
+    });
+
+    // then
+    expect(convertToTestable(variables)).to.eql([
+      { name: 'inputElement', origin: ['Subprocess_1'], scope: 'Subprocess_1' },
+    ]);
+  });
+
+});
+
+
+// helpers //////////
+
+function getRootElement(definitions) {
+  return definitions.get('rootElements')[0];
+}
+
+async function parse(xml) {
+  const moddle = new BpmnModdle({
+    zeebe: ZeebeModdle,
+  });
+
+  const { rootElement: definitions } = await moddle.fromXML(xml);
+
+  return definitions;
+}
+
+function read(path, encoding = 'utf8') {
+  return fs.readFileSync(path, encoding);
+}
+
+// converts the variables list from full moddle elements to only id, for better testability
+function convertToTestable(variables) {
+  return map(variables, function(variable) {
+    return {
+      name: variable.name,
+      origin: map(variable.origin, function(origin) {
+        return origin.id;
+      }),
+      scope: variable.scope.id
+    };
+  });
+}
