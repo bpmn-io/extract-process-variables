10cbe6bbd8cec6bf5b34a44dc351c95683de6c13
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-20 17:00:55 +0100

    test(zeebe): introduce test helper

--- test/zeebe/spec/extractors/extractInputMappingsSpec.js
+++ test/zeebe/spec/extractors/extractInputMappingsSpec.js
@@ -1,16 +1,9 @@
 import { expect } from 'chai';
 
-import { map } from 'min-dash';
-
-import { BpmnModdle } from 'bpmn-moddle';
-
-import ZeebeModdle from 'zeebe-bpmn-moddle/resources/zeebe.json' with { type: 'json' };
-
-import fs from 'fs';
-
 import extractVariables from '../../../../src/zeebe/extractors/extractInputMappings.js';
 
 import { selfAndAllFlowElements } from '../../../../src/shared/util/ElementsUtil.js';
+import { convertToTestable, getRootElement, readModel } from '../../TestHelper.js';
 
 
 describe('zeebe/extractors - input mappings', function() {
@@ -18,11 +11,9 @@ describe('zeebe/extractors - input mappings', function() {
   it('should extract variables from in mappings', async function() {
 
     // given
-    const xml = read('test/zeebe/fixtures/sub-process.input-mapping.bpmn');
+    const model = await readModel('test/zeebe/fixtures/sub-process.input-mapping.bpmn');
 
-    const definitions = await parse(xml);
-
-    const rootElement = getRootElement(definitions);
+    const rootElement = getRootElement(model);
 
     const elements = selfAndAllFlowElements([ rootElement ], false);
 
@@ -44,11 +35,9 @@ describe('zeebe/extractors - input mappings', function() {
   it('should ignore invalid input mapping', async function() {
 
     // given
-    const xml = read('test/zeebe/fixtures/errors.bpmn');
+    const model = await readModel('test/zeebe/fixtures/errors.bpmn');
 
-    const definitions = await parse(xml);
-
-    const rootElement = getRootElement(definitions);
+    const rootElement = getRootElement(model);
 
     const elements = selfAndAllFlowElements([ rootElement ], false);
 
@@ -64,37 +53,3 @@ describe('zeebe/extractors - input mappings', function() {
   });
 
 });
-
-
-// helpers //////////
-
-function getRootElement(definitions) {
-  return definitions.get('rootElements')[0];
-}
-
-async function parse(xml) {
-  const moddle = new BpmnModdle({
-    zeebe: ZeebeModdle,
-  });
-
-  const { rootElement: definitions } = await moddle.fromXML(xml);
-
-  return definitions;
-}
-
-function read(path, encoding = 'utf8') {
-  return fs.readFileSync(path, encoding);
-}
-
-// converts the variables list from full moddle elements to only id, for better testability
-function convertToTestable(variables) {
-  return map(variables, function(variable) {
-    return {
-      name: variable.name,
-      origin: map(variable.origin, function(origin) {
-        return origin.id;
-      }),
-      scope: variable.scope.id
-    };
-  });
-}
